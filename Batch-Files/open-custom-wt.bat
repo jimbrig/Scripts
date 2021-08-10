584945478556665d2359565f0593031d6293ca56
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-08-10 00:42:47 -0400

    add some system batch files

---
+++ Batch-Files/open-custom-wt.bat
@@ -0,0 +1,4 @@
+@echo off
+
+"C:\Users\jimbr\AppData\Local\Microsoft\WindowsApps\wt.exe" -p "System Monitor" --tabColor #009999 ; new-tab -p "PowerShell" -d "C:\Users\jimbr\Dev"; new-tab -p "PowerShell (No Profile)" -d "C:\Users\jimbr\Dev"; new-tab -p "Ubuntu-20.04" ; new-tab -p "Ubuntu-CommPrev" ; focus-tab -t 0
+
