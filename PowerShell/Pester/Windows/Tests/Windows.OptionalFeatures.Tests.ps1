a83b173abcde1b91496fb65dded90f9fd723853b
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-06-17 19:21:07 -0400

    feat: add powershell optional features script

--- PowerShell/Pester/Windows/Tests/Windows.OptionalFeatures.Tests.ps1
+++ PowerShell/Pester/Windows/Tests/Windows.OptionalFeatures.Tests.ps1
@@ -1,12 +1,44 @@
 BeforeDiscovery {
-    $script:OptionalFeaturesStatus = @(
-        @{FeatureName = 'Microsoft-Hyper-V-All'; MissingOK = $False; Enabled = $True }
+    $OptionalFeaturesToEnable = @(
+        HypervisorPlatform
+        VirtualMachinePlatform
+        Microsoft-Hyper-V-All
+        Microsoft-Hyper-V-Tools-All
+        Microsoft-Hyper-V-Management-PowerShell
+        Microsoft-Hyper-V-Hypervisor
+        Microsoft-Hyper-V-Services
+        Microsoft-Hyper-V-Management-Clients
+        HostGuardian
+        Containers-DisposableClientVM
+        Windows-Defender-ApplicationGuard
+        Containers
+        Containers-HNS
+        Containers-SDN
+        SmbDirect
+        Microsoft-Windows-Subsystem-Linux
+        Microsoft-Windows-Subsystem-Linux-All
+        Windows-Identity-Foundation
+        NetFx3        
+    )
+
+     
+
+    $OptionalFeaturesDesiredState = @(
+        @{FeatureName = 'Microsoft-Hyper-V-All'; State = 'Enabled'; MissingOK = $False },
+        @{FeatureName = 'Microsoft-Windows-Subsystem-Linux'; State = 'Enabled'; MissingOK = $False },
+        @{FeatureName = 'Microsoft-Windows-Subsystem-Linux-All'; State = 'Enabled'; MissingOK = $False },
+        @{FeatureName = 'Microsoft-Windows-Subsystem-Linux-WslOptionalFeature'; State = 'Enabled'; MissingOK = $False },
+        @{FeatureName = 'SearchEngine-Client-Package'; State = 'Enabled'; MissingOK = $False },
+        @{FeatureName = 'HypervisorPlatform'; State = 'Enabled'; MissingOK = $False },
+
     )
 }
 
 BeforeAll {
     $script:OptionalFeatures = Get-WindowsOptionalFeature -Online
 }
+
+$EnabledFeatures = Get-WindowsOptionalFeature -Online | Where-Object { $_.State -eq 'Enabled' }
 
 Describe "Verify Windows Optional Features Status" {
 
