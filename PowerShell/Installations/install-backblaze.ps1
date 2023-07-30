dd54f583c8f807e0246b77c15b55eb02adbe131d
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-07-30 17:04:31 -0400

    feat: update backblaze pwsh script

--- PowerShell/Installations/install-backblaze.ps1
+++ PowerShell/Installations/install-backblaze.ps1
@@ -2,4 +2,10 @@ $uri = "https://secure.backblaze.com/win32/install_backblaze.exe"
 $out = "$HOME\Downloads\Backblaze.exe"
 Invoke-WebRequest -Uri $uri -OutFile $out
 Set-Location "$HOME\Downloads"
-.\Backblaze.exe 
+.\Backblaze.exe
+
+
+$MSIUri = "https://secure.backblaze.com/win32/install_backblazemsi.msi"
+$MSIOut = "$HOME\Downloads\install_backblazemsi.msi"
+pwsh.exe -NoProfile -Command { Invoke-WebRequest -Uri $MSIUri -OutFile $MSIOut }
+msiexec.exe /I $MSIOut BZEMAIL='jimmy.briggs@jimbrig.com'
